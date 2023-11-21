514fbcb31c92db2ec48faf4a4dc08b77d23b61a5
Clément Rigo <rigoclement@mydnic.be>
2023-11-21 10:29:35 +0100

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,8 +1,23 @@
+# Restart MPD
 sudo /etc/init.d/mpd restart
-mountnas
-cd /home/pi/NasTirith/music/Musiques
-cleanFolders
-cleanNotMusic
+
+# Mount volume if necessary
+# sudo mount -o nolock {VOLUME_IP}:/path-to/music /home/pi/path-to/music
+
+# Go to dir
+cd /home/pi/path-to/music
+
+# Clean Folders (remove @eaDir folders)
+find /home/pi/path-to/music -type d -name @eaDir -print0 | xargs -0  -I {} rm -rfv {}
+
+# Remove anything that is not an mp3 file
+find /home/pi/path-to/music -type f -not -name '\''*.mp3'\'' -delete
+
+# Clear mpc playlist
 mpc clear
-mpdrestart
+
+# Restart MPD Again
+sudo /etc/init.d/mpd restart
+
+# Update mpc database
 mpc update
